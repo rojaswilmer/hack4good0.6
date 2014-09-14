bc786fa64273d7f78dc1a7a8ac39f38a9723d4b0
Rojas Wilmer <rojaswilmer@gmail.com>
2014-09-14 13:24:51 +0100

    Starting to work in the sidebar

--- application/views/layout/sidebar_layout.php
+++ application/views/layout/sidebar_layout.php
@@ -1,5 +1,10 @@
 <div class="row">
   <div class="col-lg-3 sidebar">
-  	
+      <div class="row">
+          <div class="col-lg-4"><button type="button" class="btn btn-default btn-circle btn-xl">20</i></button></div>
+          <div class="col-lg-4"><button type="button" class="btn btn-default btn-circle btn-xl">50</i></button></div>
+          <div class="col-lg-4"><button type="button" class="btn btn-default btn-circle btn-xl">99</i></button></div>
+      </div>
+
   </div>
 </div>
