57d432dcf30c4eab3f467f9d4a646043464d8110
Rojas Wilmer <rojaswilmer@gmail.com>
2014-09-14 14:38:54 +0100

    Fixing layout maps+

--- application/views/reports/dashboard.php
+++ application/views/reports/dashboard.php
@@ -1,12 +1,18 @@
 <div class="col-lg-9 {page_class}">
+    <h1>Picture from: SUMATRA, INDONESIA</h1>
     <div class="pull-left">
         <h1>Previous</h1>
     </div>
     <div class="pull-right">
         <h1>Next</h1>
     </div>
+</div>
 
-    <div>
-        <img src="http://localhost:8000/public/data/maps/test/img1.1.jpg" />
+<div class="col-lg-9">
+    <div class="col-lg-6">
+        <div id="map1" class="leaflet-container leaflet-fade-anim img-responsive" style="width: 100%; height: 500px" tabindex="0" style="position: relative;"></div>
+    </div>
+    <div class="col-lg-6">
+        <div id="map2" class="leaflet-container leaflet-fade-anim img-responsive" style="width: 100%; height: 500px" tabindex="0" style="position: relative;"></div>
     </div>
 </div>
